74b2f047825fb6d9909350846552af06e0779ecb
rachna-uva <126685027+rachna-uva@users.noreply.github.com>
2026-01-16 16:59:58 +0100

    changes to packages react

--- Dashboard Prototype_v3 - CBS C1/src/App.tsx
+++ Dashboard Prototype_v3 - CBS C1/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import { HomePage } from './components/HomePage';
 import { SearchResultsPage } from './components/SearchResultsPage';
 import { ArticlePage } from './components/ArticlePage';
