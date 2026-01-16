b1a2fd473f1f6756e90364b7f446ff32c56131df
rachna-uva <126685027+rachna-uva@users.noreply.github.com>
2026-01-16 17:09:28 +0100

    v3

--- Dashboard Prototype_v3 - CBS C1/src/components/ui/alert.tsx
+++ Dashboard Prototype_v3 - CBS C1/src/components/ui/alert.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { cva, type VariantProps } from "class-variance-authority@0.7.1";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "./utils";
 
